c02284ddd1efd5a44f858d79f10269301d49f0ce
Emma Wolff <wolff23e@mtholyoke.edu>
2019-08-19 10:10:50 -0700

    database edits- very important update

--- routes/article5.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('article5', { title: 'Article 5' });
-});
-
-module.exports = router;
