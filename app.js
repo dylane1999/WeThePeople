7ceb472b715889e0879688aabd58439b2c38289a
Dylan Edwards <dylanedwards290@gmail.com>
2019-08-19 13:19:12 -0700

    add

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json());1
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
