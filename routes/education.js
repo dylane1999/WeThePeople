cd8f958b88f988b231756a295be671bf5d372c83
Emma Wolff <wolff23e@mtholyoke.edu>
2019-08-21 15:23:32 -0700

    reformat of education page

--- routes/education.js
+++ routes/education.js
@@ -5,6 +5,7 @@ const articles = require('../database')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('education', { title: 'Learn About the Constitution', articles });
+  const article = articles.find(a => a.id === Number(req.params.id) )
 });
 
 module.exports = router;
