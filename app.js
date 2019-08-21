1be8eb9c03c0c0d2dfef646de97c008171faedbd
Emma Wolff <wolff23e@mtholyoke.edu>
2019-08-21 11:10:18 -0700

    drft 1 timeline

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session')
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var cookieSession = require("cookie-session");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var quizRouter = require('./routes/quiz');
-const articleRouter = require('./routes/article')
-var educationRouter = require('./routes/education');
+var indexRouter = require("./routes/index");
+var quizRouter = require("./routes/quiz");
+const articleRouter = require("./routes/article");
+var educationRouter = require("./routes/education");
+
+var timelineRouter = require("./routes/timeline");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
-
-
-app.use(logger('dev'));
-app.use(express.json());1
+app.use(logger("dev"));
+app.use(express.json());
+1;
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1'],
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1"],
 
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  })
+);
 
-app.use('/', indexRouter);
-app.use('/quiz', quizRouter);
-app.use('/article', articleRouter);
-app.use('/education', educationRouter);
+app.use("/", indexRouter);
+app.use("/quiz", quizRouter);
+app.use("/article", articleRouter);
+app.use("/education", educationRouter);
+app.use("/timeline", timelineRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,11 +51,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
