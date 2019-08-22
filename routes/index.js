863a09ff8fd5c398dfd5c6e946a429f24bd1df70
Dylan Edwards <dylanedwards290@gmail.com>
2019-08-21 19:40:21 -0700

    added a input for phone numbers w js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var router = express.Router();
 // const articles = require('../database')
+const saveNumber = require('../lib/saveNumber')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'We The People'});
 });
 
+
+router.post('/', function(req, res) {
+  let number = req.body.number
+
+  if(/\d{9}/.test(number)) {
+    saveNumber(number)
+    res.redirect('/')
+  } else {
+    res.send(`the number you entereed [${number}] isn't valid`)
+  }
+})
+
+
+
 module.exports = router;
+
+
