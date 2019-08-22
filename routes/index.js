dc5f09956f95d27fc54aa6cecd1e6e404b522f69
Dylan Edwards <dylanedwards290@gmail.com>
2019-08-22 12:29:07 -0700

    fixed send messafe

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 // const articles = require('../database')
 // const saveNumber = require('../lib/saveNumber')
+const sendSMS = require("../lib/sendSMS")
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -30,30 +31,32 @@ router.get("/search", function(req, res, next) {
 
 
 router.get('/send-sms', function(req, res){
-  const path = require('path')
-  const { spawn } = require('child_process');
+  // const path = require('path')
+  // const { spawn } = require('child_process');
 
   let index = req.app.get("index")
   let messages = req.app.get("messages")
-  let numbers = req.app.get("numbers").join(",")
+  let numbers = req.app.get("numbers")
 
-  let cmd = `${path.join(__dirname, "..", "bin", "send_sms")}`// ${index} "${numbers}"`
+  sendSMS(messages, index, numbers)
+  
+  // let cmd = `${path.join(__dirname, "..", "bin", "send_sms")}`// ${index} "${numbers}"`
 
   // send messages
-  const child = spawn(cmd, [index, `"${numbers}"`]);
+  // const child = spawn(cmd, [index, `"${numbers}"`]);
 
-  child.on('error', function() {
-    console.log(arguments)
-    console.log("an error occurred with send_sms")
-  })
+  // child.on('error', function() {
+  //   console.log(arguments)
+  //   console.log("an error occurred with send_sms")
+  // })
 
-  child.on('exit', function (code, signal) {
-    if(code) {
-      console.log("an error occurred with Twilio send_sms")
-    }
+  // child.on('exit', function (code, signal) {
+  //   if(code) {
+  //     console.log("an error occurred with Twilio send_sms")
+  //   }
 
-    console.log("sms sent successfully")
-  });
+  //   console.log("sms sent successfully")
+  // });
 
   // reset since we only have 10 messages
   if (index >= messages.length) {
